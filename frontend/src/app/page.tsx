106e193ad30470b21c2e6613ffcbbd0234f15dba
Anthoni Campos <anthonithomes@gmail.com>
2024-12-09 20:32:48 -0300

    Card de informações refatorado para ficar dinamico

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -9,8 +9,16 @@ export default function Home() {
       <div className="w-full flex flex-col gap-2 items-center justify-center absolute translate-y-[-70px]">
         <UserCard />
         <div className="grid grid-cols-2 gap-2 w-[80%]">
-          <InfosCard />
-          <InfosCard />
+          <InfosCard
+            cardContent="Personal Info"
+            bornAt="19 de novembro de 2003"
+            age="21 years"
+          />
+          <InfosCard
+            cardContent="Contact Info"
+            email="teste@teste.com"
+            phone1="XXXXX-XXXX"
+          />
         </div>
       </div>
     </div>
