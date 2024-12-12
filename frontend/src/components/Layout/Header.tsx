49a3e5a45117d090034c2a3c72efcac179a9ce3f
Anthoni Campos <anthonithomes@gmail.com>
2024-12-11 21:09:10 -0300

    Adicionando componente de dropdown do shadcn

--- frontend/src/components/Layout/Header.tsx
+++ frontend/src/components/Layout/Header.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import DropdownFollowers from "./DropdownFollowers";
 
 const Header = () => {
   return (
     <div className="text-white w-full p-5 bg-purple-800 shadow-xl border-b border-purple-900 absolute z-10 flex justify-between">
       <p className="cursor-default">Users_like.me</p>
-      <p>Espaço para following</p>
+      <DropdownFollowers />
     </div>
   );
 };
